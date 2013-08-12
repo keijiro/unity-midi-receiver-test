1cee3f55fcd57059d7633e1ac0bdfd2489c256be
Takahashi Keijiro <keijiro@Super-MSX.local>
2013-08-12 15:52:52 +0900

    Initial commit.

---
+++ Assets/Plugins/UnityMidiReceiver.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using System.Collections;
+
+public class UnityMidiReceiver : MonoBehaviour
+{
+    [DllImport ("UnityMIDIReceiver")]
+    private static extern void UnityMIDIReceiver_Initialize ();
+
+    [DllImport ("UnityMIDIReceiver")]
+    private static extern string UnityMIDIReceiver_GetLogText ();
+
+    string text;
+
+    void Start ()
+    {
+        UnityMIDIReceiver_Initialize ();
+    }
+
+    void Update ()
+    {
+        text = UnityMIDIReceiver_GetLogText ();
+    }
+
+    void OnGUI ()
+    {
+        GUILayout.Label (text);
+    }
+}
